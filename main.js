622c3e611a6f0b11c5b1c716cab0cf3658e62d47
CHOI55 <mac@macui-MacBook-Pro.local>
2021-03-20 21:03:36 +0900

    스크롤 액션 추가

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 (() => {
   let yOffset = 0;
+  let prevScrollHeight = 0;
+  let currnetScene = 0;
 
   const sceneInfo = [
     {
@@ -9,6 +11,13 @@
       scrollHeight: 0,
       objs: {
         container: document.querySelector("#scroll-section-0"),
+        messageA: document.querySelector("#scroll-section-0 .main-message.a"),
+        messageB: document.querySelector("#scroll-section-0 .main-message.b"),
+        messageC: document.querySelector("#scroll-section-0 .main-message.c"),
+        messageD: document.querySelector("#scroll-section-0 .main-message.d"),
+      },
+      values: {
+        messageA_opacity: [0, 1],
       },
     },
     {
@@ -47,14 +56,65 @@
         i
       ].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
     }
+    yOffset = window.pageYOffset;
+    let totalScrollHeight = 0;
+    for (let i = 0; i < sceneInfo.length; i++) {
+      totalScrollHeight += sceneInfo[i].scrollHeight;
+      if (totalScrollHeight >= pageYOffset) {
+        currnetScene = i;
+        break;
+      }
+    }
+    document.body.setAttribute("id", `show-scene-${currnetScene}`);
+  }
+  function calcValues(values, currentYOffset) {
+    let rv;
+    let scrollRatio = currentYOffset / sceneInfo[currnetScene].scrollHeight;
+
+    rv = scrollRatio * (values[1] - values[0] + values[0]);
+    return rv;
+  }
+  function playAnimation() {
+    const objs = sceneInfo[currnetScene].objs;
+    const values = sceneInfo[currnetScene].values;
+    const currentYOffset = yOffset - prevScrollHeight;
+    switch (currnetScene) {
+      case 0:
+        let messageA_opacity_in = calcValues(
+          values.messageA_opacity,
+          currentYOffset
+        );
+        objs.messageA.style.opacity = messageA_opacity_in;
+        break;
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+    }
+  }
+  function scrollLoop() {
+    prevScrollHeight = 0;
+    for (let i = 0; i < currnetScene; i++) {
+      prevScrollHeight += sceneInfo[i].scrollHeight;
+    }
+
+    if (yOffset >= prevScrollHeight + sceneInfo[currnetScene].scrollHeight) {
+      currnetScene++;
+      document.body.setAttribute("id", `show-scene-${currnetScene}`);
+    } else if (yOffset < prevScrollHeight) {
+      if (currnetScene === 0) return;
+      currnetScene--;
+      document.body.setAttribute("id", `show-scene-${currnetScene}`);
+    }
+    playAnimation();
   }
 
-  function scrollLoop() {}
-
-  window.addEventListener("resize", setLayout);
   window.addEventListener("scroll", () => {
     yOffset = window.pageYOffset;
     scrollLoop();
   });
-  setLayout();
+  window.addEventListener("load", setLayout);
+  window.addEventListener("resize", setLayout);
 })();
